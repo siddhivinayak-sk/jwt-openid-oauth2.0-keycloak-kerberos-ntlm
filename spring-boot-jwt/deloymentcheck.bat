0077b22c6f1dddd2ec4368c70cefc0ccdfefc81a
Sandeep Kumar <siddhivinayak.sk@gmail.com>
2021-12-01 05:36:03 -0800

    Update deloymentcheck.bat

--- spring-boot-jwt/deloymentcheck.bat
+++ spring-boot-jwt/deloymentcheck.bat
@@ -1,7 +1,1 @@
-@echo off
-kubectl -n bank-apps get deployment | findstr spring-boot-jwt > tmpfile
-set result="N"
-for /f "tokens=*" %%i in ('FINDSTR /C:"spring-boot-jwt" tmpfile') do (
-  set result=%%i
-)
-exit 0
+kubectl -n bank-apps delete -f spring-boot-jwt\spring-boot-jwt-k8-deployment.yaml
