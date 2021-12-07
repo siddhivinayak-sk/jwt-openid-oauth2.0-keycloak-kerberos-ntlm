6b7336f29d61984d0f75d1d22055f59843f11ad1
Sandeep Kumar <siddhivinayak.sk@gmail.com>
2021-12-07 15:13:18 +0530

    Update SpringBootHelloWorldApplication.java

--- spring-boot-jwt/src/main/java/com/sk/SpringBootHelloWorldApplication.java
+++ spring-boot-jwt/src/main/java/com/sk/SpringBootHelloWorldApplication.java
@@ -2,6 +2,16 @@ package com.sk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Arrays;
+
 
 @SpringBootApplication
 public class SpringBootHelloWorldApplication {
@@ -9,4 +19,21 @@ public class SpringBootHelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
+	
+	@Bean
+	@Primary
+	public CorsConfigurationSource corsConfigurationSource() {
+		String origins = "*";
+		final List<String> allowedOriginslist = new ArrayList<>();
+		Optional.ofNullable(origins).ifPresent(origin -> allowedOriginslist.addAll(Arrays.asList(origin.split(","))));
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(allowedOriginslist);
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "Accept"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
 }
