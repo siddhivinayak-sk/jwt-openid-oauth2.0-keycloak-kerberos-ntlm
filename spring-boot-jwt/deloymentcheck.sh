cd2776004c52d228d8f9b78c524ed45c4dc180a2
Sandeep Kumar <siddhivinayak.sk@gmail.com>
2021-12-16 17:28:55 +0530

    Update deloymentcheck.sh

--- spring-boot-jwt/deloymentcheck.sh
+++ spring-boot-jwt/deloymentcheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-minikube kubectl -- -n bank-apps delete -f spring-boot-jwt/spring-boot-jwt-k8-deployment.yaml
+kubectl -n bank-apps delete -f spring-boot-jwt/spring-boot-jwt-k8-deployment.yaml
